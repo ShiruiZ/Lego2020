28910f0657086664b85a16807a40e40815195dfc
ShiruiZ <shirui.zhang0708@gmail.com>
2020-01-10 20:20:51 -0500

    Add files via upload

---
+++ getLegoNames.py
@@ -0,0 +1,14 @@
+import urllib, re, os
+
+url = "https://brickset.com/sets/year-2020"
+content = urllib.urlopen(url).read()
+print("content\n" + content)
+keyword = "</span>\s[A-Z|a-z|\s|'|&|.]+"
+m = re.findall(keyword, content)
+_ = os.system('clear')
+print(m)
+if m:
+	for part in m:
+		print(part[8:])
+else:
+	print("Could not found")
